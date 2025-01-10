46261ed0f382643cdc96a83b4bb367b397d8987d
MohamedAhmed342 <mohamedtemo342@gmail.com>
2025-01-11 01:34:12 +0200

    handel main branch

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.all("*", (req, res, next) => {
 // Global error handling meddleware for express
 app.use(globalError);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
